4dfd22a6250461d4a2d9a670552a9bc6c20d6c61
Vinicius Lima <vinicius2014fl22@hotmail.com>
2025-04-17 14:29:54 -0300

    Colocando condição de fazer Novamente após o FIM.

--- Exercicio02.py
+++ Exercicio02.py
@@ -1,12 +1,16 @@
 #Faça um Algoritmo para receber um Número qualquer e imprimir na tela se o Número é pár ou ímpar, positivo ou negativo!
-Num = int(input("Digite um Número: "))
-if Num % 2 == 0:
-    if Num > 0:
-        print(f"O {Num}, é PAR e POSITIVO!")
+
+Novamente = "s"
+while Novamente == "s":
+    Num = int(input("Digite um Número: "))
+    if Num % 2 == 0:
+        if Num > 0:
+            print(f"O {Num}, é PAR e POSITIVO!")
+        else:
+            print(f"O {Num}, é PAR e NEGATIVO!")
     else:
-        print(f"O {Num}, é PAR e NEGATIVO!")
-else:
-    if Num > 0:
-        print(f"O {Num}, é IMPAR e POSITIVO!")
-    else:
-        print(f"{Num}, é IMPAR e NEGATIVO!")  
+        if Num > 0:
+            print(f"O {Num}, é IMPAR e POSITIVO!")
+        else:
+            print(f"{Num}, é IMPAR e NEGATIVO!")
+    Novamente = input("Deseja Verificar Novamante?(s/n) ") 
