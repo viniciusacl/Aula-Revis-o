a0f86d60b1903062c075faac577e8917398fbc4c
Vinicius Lima <vinicius2014fl22@hotmail.com>
2025-04-17 15:29:12 -0300

    #Calculando IMC do Usuário e dizendo sua condição de saúde!

---
+++ Exercicio05.py
@@ -0,0 +1,19 @@
+#Calculando IMC do Usuário e dizendo sua condição de saúde!
+
+IMC_P = float(input("Digite seu Peso: "))
+IMC_A = float(input("Digite seu Altura: "))
+IMC_T = IMC_P / (IMC_A ** 2)
+print(IMC_T)
+
+if IMC_T <= 18.6:
+    print(f"Seu IMC é de: {IMC_T:.1f} e você está ABAIXO DO PESO!")
+elif IMC_T >= 18.6 and IMC_T <= 24.9:
+    print(f"PARABÉNS, Você está no PESO IDEAL!")
+elif IMC_T >= 25.0 and IMC_T <= 29.9:
+    print(f"Você está Levemente ACIMA DO PESO!")
+elif IMC_T >= 30.0 and IMC_T <= 34.9:
+    print(f"Você está com OBESIDADE GRAU1!")
+elif IMC_T >= 35.0 and IMC_T <= 39.9:
+    print(f"Você está com OBESIDADE GRAU2!,(SEVERA)")
+else:
+    print(f"OBESIDADE GRAU3!,(MÓRBIDA)")  
