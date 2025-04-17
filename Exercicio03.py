9d004a3e6f2576c353c596b833f491af717baa9d
Vinicius Lima <vinicius2014fl22@hotmail.com>
2025-04-17 14:48:27 -0300

    Calculando quantos Salrios Min, recebe um Usuário

--- Exercicio03.py
+++ Exercicio03.py
@@ -1,4 +1,4 @@
 Valor = int(input("Digite um Valor: "))
 Antecessor = Valor - 1
 Sucessor = Valor + 1
-print(f"{Antecessor}, {Num}, {Sucessor}!")
+print(f"{Antecessor}, {Valor}, {Sucessor}!")
---
+++ Exercicio04.py
@@ -0,0 +1,5 @@
+#Calcular quantos salários Minimos recebe um usuário.
+Salario_Min = 1412
+Salario_Usu = float(input("Digite seu Salário: "))
+Qnt = Salario_Usu / Salario_Min
+print(f"O Usuário recebe,{Qnt:.1f}, Salários Min") 
